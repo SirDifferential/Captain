76f1125629fc9e18cb0c42554ab0caf865f1a8d5
Jesse Kaukonen <jesse.kaukonen@gmail.com>
2012-01-12 16:49:51 +0200

    Some build documentation

--- build.sh
+++ build.sh
@@ -1,12 +1,45 @@
 #/bin/sh
 
+echo "#################################################"
+echo "#                                               #"
+echo "# You have invoked the build script for Captain #"
+echo "#  Before mailing me about bad code, make sure  #"
+echo "#     you have all the required libraries       #"
+echo "#                                               #"
+echo "#           This project depends on:            #"
+echo "#              - SDL                            #"
+echo "#              - SDL_mixer                      #"
+echo "#              - SDL_image                      #"
+echo "#              - SDL_ttf                        #"
+echo "#              - OpenGL                         #"
+echo "#              - glew                           #"
+echo "#              - boost                          #"
+echo "#                                               #"
+echo "#   My name is Jesse Kaukonen and my email is   #"
+echo "#   my.name at the mail offered by Google       #"
+echo "#                                               #"
+echo "#################################################"
+
+echo "Press return to continue"
+
+read dummy_input
+
 echo 'Removing old files'
-rm ./run_captain
-rm ./bin/data/Captain
 cd ./src
+rm ./Kapteeni
+make clean
+
 echo 'Compiling...'
 make
-echo 'Moving executable to bin/data'
-mv ./Captain ../bin/data
-cd ..
-echo 'Run from run_captain.sh'
+
+if [ ! -f ./Kapteeni ]
+then
+	echo "There was an error in compiling. See the compiler logs above"
+	echo "In case it isn't a simple library problem, mail the developer"
+	exit 1
+else
+	echo "Build successful"
+	echo "All done!"
+	echo "Make sure to be in the ./src directory when running the binary"
+fi
+
